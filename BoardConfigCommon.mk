f8ff147a3179b9349f5704dad9642af5746b57fe
juvevood <juvemail8@gmail.com>
2019-02-27 16:47:10 +0800

    fix error

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -139,4 +139,4 @@ BOARD_VNDK_VERSION := current
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 
 # Inherit from the proprietary version
--include vendor/xiaomi/polaris/BoardConfigVendor.mk
+include vendor/xiaomi/sdm845-common/BoardConfigVendor.mk
